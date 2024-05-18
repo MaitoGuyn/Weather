d5cc8e8ab8018b255f4ad35c64174ac9e86d9908
Сафиуллин Самат <samatsafiullin7@gmail.com>
2024-05-18 09:30:05 +0300

    Добавьте файлы проекта.

---
+++ Weather/Data/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data
+{
+    public class WeatherForApp
+    {
+        public WeatherForApp(DateTime dateTime, int temperature)
+        {
+            this.dateTime = dateTime;
+            this.temperature = temperature;
+            this.WeatherStatus = this.WeatherStatus;
+        }
+        public DateTime dateTime;
+
+        public int temperature { get; set; } = 0;
+
+        public string WeatherStatus { get; set; } = "";
+
+    }
+}
---
+++ Weather/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Weather.Data;
+
+namespace Weather
+{
+    
+    public partial class MainWindow : Window
+    {
+        private List<WeatherForApp> _weather;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _weather = Data.DataContext.Weather;
+
+        }
+
+    }
+}
